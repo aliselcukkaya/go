4dcc83630e31345ae91409c79b8f34409633cd0f
aliselcukkaya <selcukkaya.ali@gmail.com>
2023-01-09 12:11:43 +0300

    first benchmark

--- tdd-with-go/iteration/repeat_test.go
+++ tdd-with-go/iteration/repeat_test.go
@@ -1,12 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	result := Repeat("a")
+	result := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if result != expected {
 		t.Errorf("expected %q got %q result", expected, result)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	result := Repeat("a", 5)
+	fmt.Println(result)
+	//Output:aaaaa
 }
