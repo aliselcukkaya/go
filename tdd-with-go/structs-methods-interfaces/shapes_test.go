d390d6df3f0a8a41150ea11a1a84751250253885
aliselcukkaya <selcukkaya.ali@gmail.com>
2023-01-10 18:17:56 +0300

    using table driven tests

--- tdd-with-go/structs-methods-interfaces/shapes_test.go
+++ tdd-with-go/structs-methods-interfaces/shapes_test.go
@@ -12,24 +12,19 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, want float64, shape Shape) {
-		t.Helper()
-		got := shape.Area()
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10, 10}, 100},
+		{Circle{10}, 314.1592653589793},
+	}
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		want := 100.0
-
-		checkArea(t, want, rectangle)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		want := 314.1592653589793
-
-		checkArea(t, want, circle)
-	})
 }
