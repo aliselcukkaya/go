99a8753b1bbf50ba7068b96d81069da042bd9054
aliselcukkaya <selcukkaya.ali@gmail.com>
2023-01-10 17:47:09 +0300

    implemented methods

--- tdd-with-go/structs-methods-interfaces/shapes_test.go
+++ tdd-with-go/structs-methods-interfaces/shapes_test.go
@@ -14,11 +14,20 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 100.0
 
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
 		}
 	})
 }
