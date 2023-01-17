3af98133103b953b9c7d910cccdb067b8e41f48f
aliselcukkaya <selcukkaya.ali@gmail.com>
2023-01-17 12:13:20 +0300

    first dictionary

---
+++ tdd-with-go/maps/dictionary_test.go
@@ -0,0 +1,44 @@
+package maps
+
+import (
+	"errors"
+	"testing"
+)
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		AssertStrings(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		AssertError(t, err, ErrNotFound)
+	})
+}
+
+func AssertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func AssertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
