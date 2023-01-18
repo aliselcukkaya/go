07462fd94d9b2b6b97f53649a09e0d833d59c662
aliselcukkaya <selcukkaya.ali@gmail.com>
2023-01-18 06:46:07 +0300

    new functionality to add words to dictionary

--- tdd-with-go/maps/dictionary.go
+++ tdd-with-go/maps/dictionary.go
@@ -2,6 +2,17 @@ package maps
 
 type Dictionary map[string]string
 
+const (
+	ErrNotFound      = DictionaryError("could not find the word you were looking for")
+	ErrAlreadyExists = DictionaryError("cannot add word because it already exists")
+)
+
+type DictionaryError string
+
+func (e DictionaryError) Error() string {
+	return string(e)
+}
+
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 
@@ -10,4 +21,18 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+		return nil
+	case nil:
+		return ErrAlreadyExists
+	default:
+		return err
+	}
 }
--- tdd-with-go/maps/dictionary_test.go
+++ tdd-with-go/maps/dictionary_test.go
@@ -1,11 +1,8 @@
 package maps
 
 import (
-	"errors"
 	"testing"
 )
-
-var ErrNotFound = errors.New("could not find the word you were looking for")
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
@@ -25,6 +22,43 @@ func TestSearch(t *testing.T) {
 
 		AssertError(t, err, ErrNotFound)
 	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "addtest"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		AssertError(t, err, nil)
+		AssertDefinition(t, dictionary, word, definition)
+	})
+	t.Run("already exists", func(t *testing.T) {
+		word := "addtest"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, definition)
+
+		AssertError(t, err, ErrAlreadyExists)
+		AssertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func AssertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
+	}
 }
 
 func AssertStrings(t testing.TB, got, want string) {
