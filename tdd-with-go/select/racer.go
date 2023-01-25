453bf7a7b7ba0cafa912326626200eefa32c2546
aliselcukkaya <selcukkaya.ali@gmail.com>
2023-01-25 15:32:14 +0300

    refactor

--- tdd-with-go/select/racer.go
+++ tdd-with-go/select/racer.go
@@ -1,13 +1,25 @@
 package racer
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-func Racer(firstURL, secondURL string) (winner string) {
+var tenSecondTimer = 10 * time.Second
+
+func Racer(firstURL, secondURL string) (winner string, error error) {
+	return ConfigurableRacer(firstURL, secondURL, tenSecondTimer)
+}
+
+func ConfigurableRacer(firstURL, secondURL string, timeout time.Duration) (winner string, error error) {
 	select {
 	case <-ping(firstURL):
-		return firstURL
+		return firstURL, nil
 	case <-ping(secondURL):
-		return secondURL
+		return secondURL, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", firstURL, secondURL)
 	}
 }
 
