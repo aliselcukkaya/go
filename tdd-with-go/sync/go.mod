bc0e7945708813a5dbf8d63d187aa6c90d6f8d22
aliselcukkaya <selcukkaya.ali@gmail.com>
2023-02-13 12:00:12 +0300

    sync

---
+++ tdd-with-go/sync/go.mod
@@ -0,0 +1,3 @@
+module sync_package
+
+go 1.19
---
+++ tdd-with-go/sync/sync.go
@@ -0,0 +1,17 @@
+package sync_package
+
+import "sync"
+
+type Counter struct {
+	m     sync.Mutex
+	value int
+}
+
+func (c *Counter) Inc() {
+	c.m.Lock()
+	defer c.m.Unlock()
+	c.value++
+}
+func (c *Counter) Value() int {
+	return c.value
+}
